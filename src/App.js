953d44756f54c17b8f02fea211dc9c1da55e28bd
JasmineBabaYara <babayarajasmine@gmail.com>
2023-11-24 03:46:43 -0800

    initialization of project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./pages/landingPage/header.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
