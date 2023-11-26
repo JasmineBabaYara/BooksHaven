c2cf404207f82c584965e8e8e130cb069ff974ef
JasmineBabaYara <babayarajasmine@gmail.com>
2023-11-26 13:15:00 -0800

    add data

---
+++ src/utils/data.jsx
@@ -0,0 +1,14 @@
+import book1 from "../images/img/books/book1.jpg";
+
+const data =[
+    {
+        id: 1,
+        photo: book1,
+        title: 'Wings Of Fire: The dragon Prophecy',
+        year_of_publication: '2012',
+        author: 'Tui T. Sutherland',
+        country_of_author: 'New York',
+    }
+]
+
+export default data;
