1b9701bf53a9c2b8b99edc47d4e78c10c663c0a2
JasmineBabaYara <babayarajasmine@gmail.com>
2023-11-27 06:05:45 -0800

    render footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Header from "./pages/landingPage/header";
 import Content from "./pages/landingPage/content";
+import Sort from "./pages/landingPage/sort";
+import BooksDisplay from './pages/landingPage/booksDisplay';
+import Footer from "./pages/landingPage/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Content />
+      <Sort />
+      <BooksDisplay />
+      <Footer />
     </div>
   );
 }
