06a757c1cbd5f48ce1fdef0fb64e23e730916080
JasmineBabaYara <babayarajasmine@gmail.com>
2023-11-27 09:24:00 -0800

    make the content page responsive

--- src/pages/landingPage/content.jsx
+++ src/pages/landingPage/content.jsx
@@ -5,27 +5,29 @@ import book4 from "../../images/img/books/book4.webp";
 
 export default function content() {
   return (
-    <div className="flex flex-row justify-between items-center w-full mt-4">
-      <div className="w-5/12 text-left">
-        <p className="contentHeader">Books Haven, Home of the Best Books</p>
-        <p className="mt-4 ">
+    <div className="flex laptop:flex-row flex-col justify-between items-center w-full mt-4">
+      <div className="laptop:w-5/12 w-12/12 text-left">
+        <p className="contentHeader tablet:text-6xl text-3xl font-bold">
+          Books Haven, Home of the Best Books
+        </p>
+        <p className="mt-4 tablet:text-base text-sm">
           Vestibulum lorem erat, interdum non rhoncus nec, porttitor quis elit.
           Integer dignissim arcu nec elit ultricies, vel semper massa eleifend.
           Sed non arcu at purus sodales ultricies. Aliquam fermentum nunc a enim
           congue, at egestas erat congue.
         </p>
       </div>
-      <div className="w-5/12 flex gap-10 items-center">
+      <div className="laptop:w-5/12 w-full tablet:flex laptop:mt-0 mt-10 tablet:flex-row flex-col laptop:gap-10 mob:hidden items-center justify-between">
         <div className="">
-          <img src={book4} alt="book1" className="h-72 w-52 rounded-lg"/>
+          <img src={book4} alt="book1" className="h-72 w-52 rounded-lg" />
         </div>
-        <div>
-        <div className="">
-          <img src={book3} alt="book3" className="h-72 w-52 rounded-lg"/>
-        </div>
-        <div className="mt-4">
-          <img src={book2} alt="book2" className="h-72 w-52 rounded-lg"/>
-        </div>
+        <div className="flex laptop:flex-col tablet:flex-row flex-col w-2/3 items-center justify-between">
+          <div className="">
+            <img src={book3} alt="book3" className="h-72 w-52 rounded-lg" />
+          </div>
+          <div className="laptop:mt-4">
+            <img src={book2} alt="book2" className="h-72 w-52 rounded-lg" />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/landingPage/footer.jsx
+++ src/pages/landingPage/footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import Logo from "../../images/img/logo.png";
+import Logo from "../../images/img/logoFooter.png";
+import { RiFontFamily } from '../../../node_modules/react-icons/ri/index.esm';
 
 export default function footer() {
   return (
+    <div className='mt-14 p-10 flex flex-col items-center justify-center '>
     <div className="h-32 w-full flex items-center justify-center ">
-      <img src={Logo} alt='logo' className="h-32 w-32" />
+      <img src={Logo} alt='logo' className="h-40 w-52" />
+      <div className='flex items-center justify-center gap-6' >
+      <p className="footerBooksTxt text-7xl">BOOKS</p>
+      <p className="footerHavenTxt text-7xl">HAVEN</p>
+      </div>
+    </div>
+    <p className='text-xs text-slate-500 font-extralight'>© Copyright 2023 Books Haven</p>
     </div>
   )
 }
